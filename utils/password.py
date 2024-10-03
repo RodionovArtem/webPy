3a2e180fd58e339c9870fa5eee7409915a4fc16d
Rodionov Artem <artem07052016@mail.ru>
2024-10-03 11:06:57 +0300

    lab_4

--- utils/password.py
+++ utils/password.py
@@ -1,9 +1,12 @@
-from passlib.context import CryptContext
+import bcrypt
 
-pwd_context = CryptContext(schemes=["bcrypt"])
+def hash_password(password):
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
+    return hashed_password
 
-def hash_password(password: str):
-    return pwd_context.hash(password)
-
-def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password, hashed_password)
+def verify_password(plain_password, hashed_password):
+    password_byte_enc = plain_password.encode('utf-8')
+    hpwd = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(password = password_byte_enc , hashed_password = hpwd)
