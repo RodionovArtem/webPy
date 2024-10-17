43260ef82de9541f474724e8c91e96d42f8a50a1
Rodionov Artem <artem07052016@mail.ru>
2024-10-17 19:00:17 +0300

    lab_6

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from handlers import users_handler
 from schemas.user import UserCreate, UserAuthorize
 from database import metadata, engine, database
 from models import users
 from models.Users import UserRole
+from utils.token import validate_token_and_role
 
 metadata.create_all(bind=engine)
 
@@ -28,3 +29,11 @@ async def create_user(user: UserCreate, role: UserRole):
 @app.put("/users/authorize/")
 async def authorize_user(user: UserAuthorize):
     return await users_handler.authorize_user(user, database)
+
+@app.get("/with-credentials")
+async def check_credentials(user = Depends(validate_token_and_role([ "approved_user", "admin"]))):
+    return {"msg": "Welcome allowed user"}
+
+@app.get("/without-credentials")
+async def check_credentials():
+     return {"msg": "Welcome all"}
--- utils/token.py
+++ utils/token.py
@@ -1,6 +1,8 @@
-from jose import jwt
-from typing import Optional
+from jose import jwt, JWTError
+from typing import Optional, List
 from datetime import datetime,timedelta
+from fastapi.security import OAuth2PasswordBearer, HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import Depends, HTTPException, status
 
 SECRET_KEY = "web-tech-2"
 ALGORITHM = "HS256"
@@ -15,3 +17,30 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+oauth2_scheme = HTTPBearer()
+
+def validate_token_and_role(required_roles: List[str]):
+    def token_validator(token: HTTPAuthorizationCredentials = Depends(oauth2_scheme)):
+        token = token.credentials
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            name = payload.get("sub")
+            role = payload.get("role") 
+            if name is None or role is None:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="User or UserRole is not identified"
+                )
+            if role not in required_roles:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Your role is not valid fot this request"
+                )
+            return {"name": name, "role": role}
+        except JWTError:
+            raise HTTPException(
+            status_code=401,
+            detail = "Token is not valid"
+        )
+    return token_validator
